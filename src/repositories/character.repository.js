df6fab52c9c1af2dd054ab1b04124baef13d7d1c
Pablo Tedesco <tedesco8@hotmail.com>
2021-04-09 21:37:39 -0300

    sobrescribiendo metodo getAll de clase base en character.repository

--- src/repositories/character.repository.js
+++ src/repositories/character.repository.js
@@ -1,31 +1,37 @@
-const BaseRepository = require("./base.repository")
-let _character = null
-let _film = null
+const BaseRepository = require("./base.repository");
+let _character = null;
+let _film = null;
 
-class CharacterRepository extends BaseRepository{
-    constructor({Character, Film}) {
-        super(Character)
-        _character = Character
-        _film = Film
-    }
+class CharacterRepository extends BaseRepository {
+  constructor({ Character, Film }) {
+    super(Character);
+    _character = Character;
+    _film = Film;
+  }
 
-    async get(id) {
-        return await _character.finOne({
-            where: {id:id},
-            include: [
-                {
-                  model: _film,
-                  as: "film",
-                },
-            ],
-        })
-    }
+  async get(id) {
+    return await _character.finOne({
+      where: { id: id },
+      include: [
+        {
+          model: _film,
+          as: "film",
+        },
+      ],
+    });
+  }
 
-    async getCharacterByName(name) {
-        return await _character.finOne({
-            where: {name:name}
-        })
-    }
+  async getAll() {
+    return await this.model.findAll({
+      attributes: ["name", "image"],
+    });
+  }
+
+  async getCharacterByName(name) {
+    return await _character.finOne({
+      where: { name: name },
+    });
+  }
 }
 
-module.exports = CharacterRepository
+module.exports = CharacterRepository;
